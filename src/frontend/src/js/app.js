af5e2017248be20588fd7b966fc8136fb42e29de
Omer Ben-Amram <omerbenamram@gmail.com>
2015-05-08 16:35:45 +0300

    simple web interface to view a test json

--- src/frontend/src/js/app.js
+++ src/frontend/src/js/app.js
@@ -1,3 +1,18 @@
-/**
- * Created by Omer on 5/2/2015.
- */
+'use strict';
+
+var app = angular.module('torrentSearcher', [
+		// Angular Libraries.
+		'ngRoute', 'ngAnimate', 'ngResource', 'ngSanitize',
+
+		// App.
+		'torrentSearcher',
+
+		// External Libs.
+		'ui.bootstrap', 'ui.router', 'ui.utils'
+	]);
+
+app.controller('torrentTable', ['$scope','$http', function($scope,$http) {
+    $http.get('test/test.json').success(function(data) {
+        $scope.results = data;
+    });
+}]);
--- src/server.py
+++ src/server.py
@@ -1,18 +1,31 @@
-from flask import Flask, render_template, json
+import os
+from flask import Flask, send_file, send_from_directory
 
-STATIC_FOLDER = 'frontend/src/static'
+BUILD_PATH = './frontend/build/'
+TEST_FOLDER = './frontend/src/static/'
 
-app = Flask(__name__, template_folder='frontend/build/')
+app = Flask(__name__, instance_path=os.path.dirname(__file__), static_folder=BUILD_PATH, instance_relative_config=True)
 app.debug = True
 
 
 @app.route('/')
 def hello_word():
-    return render_template('index.html')
+    return send_file(app.static_folder + '/index.html')
+
+
+@app.route('/media/build/<path:path>')
+def send_js(path):
+    return send_from_directory(app.static_folder, path)
+
+
+@app.route('/test/<path:path>')
+def send_from_test(path):
+    return send_from_directory(TEST_FOLDER, path)
 
 
 def main():
     app.run()
+
 
 if __name__ == "__main__":
     main()
