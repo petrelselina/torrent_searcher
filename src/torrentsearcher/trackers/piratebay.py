bb691c9fe8d6681a3e4c75ef6f0d84feb683257c
Omer Ben-Amram <omerbenamram@gmail.com>
2015-07-11 19:32:23 +0300

    more columns

--- src/torrentsearcher/trackers/piratebay.py
+++ src/torrentsearcher/trackers/piratebay.py
@@ -1,12 +1,14 @@
 import logbook
-from lxml.etree import XPath
 import pandas
+import re
 
 from torrentsearcher.base.torrent_searcher import TorrentSearcher
 
 
 logger = logbook.Logger(__name__)
 
+PIRATEBAY_SIZE_REGEX = "([\d.]+)([M,G]).?B"
+size_re = re.compile(PIRATEBAY_SIZE_REGEX, re.IGNORECASE | re.UNICODE)
 
 class PirateBaySearcher(TorrentSearcher):
     base_url = "https://thepiratebay.se"
@@ -17,6 +19,20 @@ class PirateBaySearcher(TorrentSearcher):
 
     def login(self, username, password):
         return
+
+    @staticmethod
+    def get_size_from_name_string(name):
+        size_tuple = size_re.findall(name)[0]
+        size = None
+
+        if not size_tuple:
+            return size
+        if size_tuple[1] == 'M':
+            size = float(size_tuple[0])
+        if size_tuple[1] == 'G':
+            size = float(size_tuple[0])*1024
+
+        return size
 
     def query_tracker(self, term, categories=()):
         query_term_url = self.query_url + term
@@ -31,4 +47,8 @@ class PirateBaySearcher(TorrentSearcher):
         df.dropna(axis=1, how='any', inplace=True)
         df.columns = ['type', 'name', 'seeders', 'leechers']
         df.drop('type', axis=1, inplace=True)
+
+        df['name'] = df.name.apply(lambda x: x.replace(u'\xa0', u''))
+        df['size'] = df.name.apply(self.get_size_from_name_string)
+
         return df.to_json()
